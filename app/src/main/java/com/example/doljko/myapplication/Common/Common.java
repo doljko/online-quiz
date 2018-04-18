7b8e705a437162405b9232ae0192eb33a7d5a52d
Doljko <doljko_tv@yahoo.com>
2018-04-18 12:50:38 +0900

    reSelectet fixed by selected listener

---
+++ app/src/main/java/com/example/doljko/myapplication/Common/Common.java
@@ -0,0 +1,9 @@
+package com.example.doljko.myapplication.Common;
+
+import com.example.doljko.myapplication.Model.User;
+
+public class Common {
+
+    public static String categoryId;
+    public static User currentUser;
+}
--- app/src/main/java/com/example/doljko/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/doljko/myapplication/HomeActivity.java
@@ -1,6 +1,4 @@
 package com.example.doljko.myapplication;
-
-
 import android.support.v4.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,29 +6,22 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-
-
 public class HomeActivity extends AppCompatActivity {
-
     BottomNavigationView bottomNavigationView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         bottomNavigationView= (BottomNavigationView)findViewById(R.id.navigation);
-
-        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onNavigationItemReselected(@NonNull MenuItem item) {
-
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment selectedFragment = null;
                 switch (item.getItemId())
                 {
                     case R.id.action_category:
-                       selectedFragment = CategoryFragment.newIntance();
-                       break;
+                        selectedFragment = CategoryFragment.newIntance();
+                        break;
                     case R.id.action_ranking:
                         selectedFragment = RankingFragment.newIntance();
                         break;
@@ -38,13 +29,11 @@ public class HomeActivity extends AppCompatActivity {
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 transaction.replace(R.id.frame_layout,selectedFragment);
                 transaction.commit();
+                return true;
             }
         });
-
         setDefaultFragment();
-
     }
-
     private void setDefaultFragment() {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_layout,CategoryFragment.newIntance());
